6a10640891459f73192b81d95945fff4bf2ac3f8
TanakaMiki-Altissima <tanaka.miki@altissima.jp>
2026-03-23 16:44:49 +0900

    Sidebar、Headerher編集

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,16 +3,15 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-gray-300 flex-row p-2 ">
+    <header className="flex items-center justify-between shadow-[0_4px_8px_rgba(0,0,0,0.15)] flex-row p-1 pl-48">
       <div className="flex items-center gap-5">
         <h1 className="text-2xl font-semibold text-gray-400">管理CMS</h1>
       </div>
-      <p className="text-md text-gray-500">admin:Aテスト本社</p>
-      <FontAwesomeIcon
-                    icon={faUser}
-                    className="text-gray-500"
-                  />
-      <p className="text-md text-gray-500">テスト管理者 ユーザー</p>
+      <div className="flex items-center gap-3">
+        <p className="text-sm text-gray-500">admin:Aテスト本社</p>
+        <FontAwesomeIcon icon={faUser} className="text-gray-500" />
+        <p className="text-sm text-gray-500">テスト管理者 ユーザー</p>
+      </div>
     </header>
   );
 }
